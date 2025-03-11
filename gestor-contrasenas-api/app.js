0fbd6d5634b6eaa5a9c5fe3978bc68711283eafc
Pelusa <agustinlorenzatto@gmail.com>
2025-03-11 09:01:40 -0300

    Poniendo el front end en el backedn para produccion

--- gestor-contrasenas-api/app.js
+++ gestor-contrasenas-api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -11,9 +12,15 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use( express.static('public'));
+
 app.use('/gestor/users', require('./routes/user.routes'));
 app.use('/gestor/passwords', require('./routes/password.routes'));
 
+
+app.use('*', (req, res) => {
+    res.sendFile( path.join(__dirname, 'public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor Corriente en el puerto ${PORT}`);
