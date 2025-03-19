7b7f24a43a9e4b007ca26a9c2b68e0e6ae566a58
Pelusa-not <agustinlorenzatto@gmail.com>
2025-03-19 17:13:36 -0300

    Volvi mas responsivo el codigo de gestor de app

--- gestor-contrasenas/src/helpers/desencriptarPassword.js
+++ gestor-contrasenas/src/helpers/desencriptarPassword.js
@@ -4,7 +4,7 @@ import { getEnvVariables } from "../helpers/index";
 const { VITE_API_PASSWORDKEY: passwordKey } = getEnvVariables();
 
 export const desencriptar = (password) => {
+    console.log(CryptoJS.AES.decrypt(password, passwordKey).toString(CryptoJS.enc.Utf8));
     const decryptedPassword = CryptoJS.AES.decrypt(password, passwordKey)?.toString(CryptoJS.enc.Utf8);
-
     return decryptedPassword;
 };
